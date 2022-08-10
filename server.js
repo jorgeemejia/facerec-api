5105063d6bb9f14f03bc27f2a16333f81753d991
jorgeemejia <jorgeemejia@csu.fullerton.edu>
2022-08-10 16:57:24 -0700

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-metric-69192',
     //   port : 3306,
       user : 'postgres',
       password : 'joemama',
