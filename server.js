98bbaa78be1cc061be7f77684a318cde98c6c134
Jorge <jorgeemejia@csu.fullerton.edu>
2023-04-27 21:01:55 -0700

    updating knex

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'postgresql-cubed-05336',
-        user: '',
-        password : '',
-        database : 'smart-brain'
+        host: process.env.DATABASE_URL,
+        ssl: true
     },
   });
 
